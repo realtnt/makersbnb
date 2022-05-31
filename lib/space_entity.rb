a022e535da53027e0865801ebe1361320fa7de20
Matt Clements <matthewlclements@gmail.com>
2022-05-31 11:10:15 +0100

    Created entity and model specs for space and user

---
+++ lib/space_entity.rb
@@ -0,0 +1,43 @@
+class SpaceEntity
+    def initialize(
+        title, price, description, 
+        date_from, date_to, host_id = nil, id = nil
+        )
+
+        @title = title
+        @price = price
+        @description = description
+        @date_from = date_from
+        @date_to = date_to
+        @host_id = host_id
+        @id = id
+    end
+
+    def title
+        return @title
+    end
+
+    def price
+        return @price
+    end
+    
+    def description
+        return @description
+    end
+
+    def date_from
+        return @date_from
+    end
+
+    def date_to 
+        return @date_to
+    end
+
+    def host_id
+        return @host_id
+    end
+
+    def id
+        return @id
+    end
+end
